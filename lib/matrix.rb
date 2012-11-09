d73142dd53c8a9b1ba59752205504fdc6140ebf9
oddworldng <el.lobo.4ever@gmail.com>
2012-11-09 21:07:48 +0000

    commit

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,104 +1,17 @@
 #!/usr/bin/ruby
 
-class NumerosRacionales
+class Matrix
 
-	attr_accessor :numerador, :denominador
-
-	def initialize(x,y)
-		@numerador = x
-		@denominador = y
+	def sumaenteros
 	end
 
-	def num
-		numerador
+	def sumaracionales
 	end
 
-	def denom
-		denominador
-	end
-
-	def numeroracional
-		return numerador/denominador
+	def multenteros
 	end
 	
-	def mostrarab # Muestra la fracción en forma a/b
-		puts "#{numerador}/#{denominador}"
-	end
-
-	def mostrarfloat 
-		puts numeroracional
-	end
-
-	def ==(a,b,c,d)
-		if ((a/b) == (c/d))
-			return "Son iguales"
-		else
-			return "Son distintas"
-		end
-	end
-
-	def vabsoluto
-		return numeroracional.abs
-	end
-	
-	def reciproco
-		return 1/numeroracional
-	end
-
-	def -(a,b)
-		return "#{b}/#{a}" 
-	end
-
-	def +(a,b,c,d)
-		return (a/b)+(c/d)
-	end
-
-	#def -(x,y,z,w)
-	#	return (x/y)-(z/w)
-	#end
-
-	def *(a,b,c,d)
-		return (a/b)*(c/d)
-	end
-
-	def /(a,b,c,d)
-		return (a/b)/(c/d)
-	end
-
-	def %(a,b,c,d)
-		return (a/b)%(c/d)
-	end
-
-	def <(a,b,c,d)
-		if (a/b)<(c/d)
-			return "true"
-		else
-			return "false"
-		end
-	end
-
-	def >(a,b,c,d)
-		if (a/b)>(c/d)
-			return "true"
-		else
-			return "false"
-		end
-	end
-
-	def <=(a,b,c,d)
-		if (a/b)<(c/d)
-			return "true"
-		else
-			return "false"
-		end
-	end
-
-	def >=(a,b,c,d)
-		if (a/b)>(c/d)
-			return "true"
-		else
-			return "false"
-		end
+	def multracionales
 	end
 
 end	
